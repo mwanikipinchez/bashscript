fb0abec7de4e0489b8fb93e03936f3231a96b618
Peter Mwaniki <mwanikipinchez@gmail.com>
2023-02-28 08:44:19 +0300

    Script to run basic navigation commands

---
+++ trial.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "This is your directories"
+ls -l
+echo "This is the current  user who is login"
+whoami
+
+
+echo "chmod 777 helloworld.sh"
+chmod 777 helloworld.sh
+
+ls -l
+
+echo "The hostname is: "
+hostname
+
+echo "thank you"
